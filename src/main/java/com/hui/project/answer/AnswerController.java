73357b0058b9a9cee995f713ea51c9002f53d8b9
changhui98 <net_0102@naver.com>
2024-01-01 16:56:40 +0900

    BootStrap Css , Board Detail

--- src/main/java/com/hui/project/answer/AnswerController.java
+++ src/main/java/com/hui/project/answer/AnswerController.java
@@ -17,12 +17,14 @@ public class AnswerController {
 
     private final QuestionService questionService;
 
+    private final AnswerService answerService;
+
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
                                @RequestParam(value = "content") String content){
 
         Question question = this.questionService.getQuestion(id);
-        // TODO : 답변을 저장한다 (답변을 저장하는 코드는 아직 작성 전이므로 주석 처리)
+        this.answerService.create(question, content);
         return String.format("redirect:/question/detail/%s", id);
     }
 
