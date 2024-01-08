685f3920c86a7efc99b857be43f9e40c7772d26c
changhui98 <net_0102@naver.com>
2024-01-09 01:13:52 +0900

    게시글, 댓글 수정 및 삭제 기능 구현

--- src/main/java/com/hui/project/answer/AnswerService.java
+++ src/main/java/com/hui/project/answer/AnswerService.java
@@ -1,11 +1,14 @@
 package com.hui.project.answer;
 
+import com.hui.project.DataNotFoundException;
 import com.hui.project.question.Question;
 import com.hui.project.user.SiteUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,5 +24,26 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
     }
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        }else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer){
+        this.answerRepository.delete(answer);
+    }
+
+
 
 }
